1379759fcc03e1f40cd68604fc97b57275371f65
davronbek <davronbekmamatqulov72@gmail.com>
2026-07-28 01:06:06 +0500

    Fix: Add Netlify Next.js plugin config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Netlify va Vercel uchun mos marshrutlash
+  trailingSlash: false,
 };
 
 export default nextConfig;
